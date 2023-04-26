3a02009df95cbfaa804105f7ca728302d109f69c
Alvaro Labanca <105125713+kaponero@users.noreply.github.com>
2023-04-26 20:44:23 -0300

    Primer comit

---
+++ Tema1/src/com/company/Tipos.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Tipos {
+    public static void main(String[] args) {
+        // enteros
+        byte number1 = 1; //1 byte
+        System.out.println(number1);
+        short number2 =2;//2 byte
+        System.out.println(number2);
+        int  number3 = 3;//4 byte
+        System.out.println(number3);
+        long number4 = 4;//8 byte
+        System.out.println(number4);
+
+        //flotantes
+        float decimal1= 4.9f;
+        System.out.println(decimal1);
+        double decimal2= 9.99d;
+        System.out.println(decimal2);
+
+        //caracter
+        char caracter1 = 'a';
+        System.out.println(caracter1);
+        String caracter2= "Alvaro";
+        System.out.println(caracter2);
+
+        //boleanos
+        boolean verdadero = true;
+        System.out.println(verdadero);
+        boolean falso = false;
+        System.out.println(falso);
+    }
+}
